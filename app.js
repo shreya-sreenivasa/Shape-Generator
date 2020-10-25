c55a8e5bdca9aaffca3ddcfc866b120f53fd5d98
shreya-sreenivasa <69913569+shreya-sreenivasa@users.noreply.github.com>
2020-10-25 12:40:41 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const button=document.getElementById("button")
+
+const circle=document.getElementById("circle")
+const square=document.getElementById("square")
+const rectangle=document.getElementById("rectangle")
+
+const box = document.getElementById("box")
+
+let oldValue = 0 
+let numberInTheShape = 1 
+
+button.onclick=()=>{
+
+    let n = document.getElementById("number").value;
+    n = Number(oldValue) + Number(n)
+
+    for(let j=numberInTheShape; j<= n; j++) {
+        var shape = document.createElement("div")
+        shape.innerHTML += numberInTheShape;
+        if(circle.checked) {
+            shape.classList.add("circle")
+        } 
+        else if(square.checked) {
+            shape.classList.add("square")
+        } 
+        else if(rectangle.checked) {
+            shape.classList.add("rectangle")
+        }
+        box.appendChild(shape);
+        numberInTheShape++;
+        oldValue = box.lastElementChild.innerHTML;
+    }
+}
